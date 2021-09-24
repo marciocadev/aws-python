cfe6d69e7adf8ff0e2d8cc696c9da8ac75f48316
Marcio Cruz de Almeida <marcio.almeida@redspark.local>
2021-09-24 15:34:11 -0300

    ajustes nos testes

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
+# Search versions
+https://www.npmjs.com/package/aws-cdk
+
+
 # AWS-CDK Version 1
+```
 npm install -g aws-cdk@latest
+```
 
 # AWS-CDK Version 2
-npm install -g aws-cdk@2.0.0-rc.1
+```
+npm install -g aws-cdk@2.0.0-rc.23
+```
--- v2/example-test/unittests/infra_tests/test_lambdas.py
+++ v2/example-test/unittests/infra_tests/test_lambdas.py
@@ -1,24 +1,30 @@
 import aws_cdk as cdk
 from example_test.example_test_stack import ExampleTestStack
-import pytest
 from unittest import TestCase
 
 
-class TestLambda(TestCase):
+class TestLambdas(TestCase):
 
-    @pytest.fixture(autouse=True)
-    def init(self):
+    @classmethod
+    def setUpClass(cls):
+        print("setUpClass")
         # GIVEN
-        self.app = cdk.App()
+        cls.app = cdk.App()
         # WHEN
-        ExampleTestStack(scope=self.app, construct_id="Test")
+        ExampleTestStack(scope=cls.app, construct_id="Test")
         # THEN
+        cls.template = cls.app.synth().get_stack_by_name("Test").template
+        cls.functions = [ \
+            resource for resource in cls.template['Resources'].values() \
+            if resource['Type'] == 'AWS::Lambda::Function']
+
+    def test_lambda_count(self):
+        self.app = cdk.App()
+        ExampleTestStack(scope=self.app, construct_id="Test")
         self.template = self.app.synth().get_stack_by_name("Test").template
         self.functions = [ \
             resource for resource in self.template['Resources'].values() \
             if resource['Type'] == 'AWS::Lambda::Function']
-
-    def test_lambda_count(self):
         self.assertEqual(len(self.functions), 1)
 
     def test_lambda_name(self):
